be912ae7f5cc0540d9f750e3c23f973ae09f51f3
karlheinzniebuhr <karl.booklover@gmail.com>
2020-07-08 21:53:39 -0400

    error handling & disabled insecure request warning

--- contraloria_scraper.py
+++ contraloria_scraper.py
@@ -1,6 +1,8 @@
 # autores: @quanturtle, @karlpy
 
 import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import lxml.html as lh
 
 controlaria_urls = open('contraloria_urls.txt', 'r')
@@ -8,27 +10,31 @@ URLS = controlaria_urls.readlines()
 session = requests.Session()
 
 for index, url in enumerate(URLS):
-    url = url.strip()
-    print(f'Downloading URL {index+1}: {url}')
+    try:
+        url = url.strip()
+        print(f'Downloading URL {index+1}: {url}')
 
-    # getting the session cookie
-    get_response = session.get(url, verify=False)
+        # getting session cookie
+        get_response = session.get(url, verify=False)
 
-    # getting the token from html form
-    doc = lh.fromstring(get_response.content)
-    token = doc.xpath('//*[@id="phocadownloadform"]/input[4]')[0].name
-    file_ref = url.split('/')[-1].split('-')[0]
+        # getting token from html form
+        doc = lh.fromstring(get_response.content)
+        token = doc.xpath('//*[@id="phocadownloadform"]/input[4]')[0].name
+        file_ref = url.split('/')[-1].split('-')[0]
 
-    payload = {
-        'submit': 'Descarga',
-        'license_agree': '1',
-        'download': file_ref,
-        token: '1'
-    }
+        payload = {
+            'submit': 'Descarga',
+            'license_agree': '1',
+            'download': file_ref,
+            token: '1'
+        }
 
-    # make POST request with session cookie
-    post_response = session.post(url, data=payload)
+        # make POST request with session cookie
+        post_response = session.post(url, data=payload)
 
-    filename = f"contraloria/{url.split('/')[-1].replace('-pdf','')}.pdf"
-    with open(filename, 'wb') as outfile:
-        outfile.write(post_response.content)
+        filename = f"contraloria/{url.split('/')[-1].replace('-pdf','')}.pdf"
+        with open(filename, 'wb') as outfile:
+            outfile.write(post_response.content)
+    except Exception as ex:
+        print(ex)
+        continue
